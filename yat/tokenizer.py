7a9dfcf53adcfb83d686ea52fefc94ac341432f9
yag_ays <yanagi.ayase@gmail.com>
2018-06-13 22:55:44 +0900

    change word to (surface, feature)

--- yat/tokenizer.py
+++ yat/tokenizer.py
@@ -1,20 +1,28 @@
 from collections import defaultdict
+from collections import namedtuple
 
 import MeCab
 
 
 class Tokenizer():
     def __init__(self):
+        self.node = namedtuple("Node", ("surface", "feature"))
         self.token2id = defaultdict(lambda: -1)
-        self.id2token = defaultdict(lambda: "UNK")
+        self.id2token = defaultdict(lambda: self.node("UNK", "未知語"))
         self.set_token = set()
         self.num_id = 0
 
-        self._wakati = MeCab.Tagger("-Owakati")
-        self._wakati.parse("")
+        self._mecab = MeCab.Tagger()
+        self._mecab.parse("")
 
     def tokenize(self, text):
-        return self._wakati.parse(text).rstrip().split(" ")
+        res = []
+        for node in self._mecab.parse(text).split("\n"):
+            n = node.split("\t")
+            if n == ["EOS"]:
+                break
+            res.append(self.node(n[0], n[1].split(",")[0]))
+        return res
 
     def fit_on_text(self, text):
         words = self.tokenize(text)
@@ -37,7 +45,7 @@ class Tokenizer():
         return [self.text_to_sequence(text) for text in texts]
 
     def sequence_to_text(self, sequence):
-        return "".join([self.id2token[s] for s in sequence])
+        return "".join([self.id2token[s].surface for s in sequence])
 
     def sequences_to_texts(self, sequences):
         return ["".join(self.sequence_to_text(sequence)) for sequence in sequences]
@@ -45,14 +53,14 @@ class Tokenizer():
     def save_as_text(self, filename):
         with open(filename, "w") as f:
             for w, w_id in self.token2id.items():
-                f.write("\t".join([str(w_id), w]) + "\n")
+                f.write("\t".join([str(w_id), w.surface, w.feature]) + "\n")
 
     def load_from_text(self, filename):
         with open(filename) as f:
             for line in f:
                 l = line.strip().split("\t")
                 w_id = int(l[0])
-                w = l[1]
+                w = self.node(l[1], l[2])
 
                 self.token2id[w] = w_id
                 self.id2token[w_id] = w
